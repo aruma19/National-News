8d7118649c19dea46e12302d416ca7b224c32747
aruma19 <muhammadalmasfarrosd@gmail.com>
2025-05-27 16:03:05 +0700

    halo

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,3 +1,3 @@
-//export const BASE_URL = "https://notes-backend-farros-572347789402.us-central1.run.app"; 
+export const BASE_URL = "https://project-backend-572347789402.us-central1.run.app/"; 
 // utils.js
-export const BASE_URL = "http://localhost:5003"; // sesuaikan dengan port backend
+//export const BASE_URL = "http://localhost:5003"; // sesuaikan dengan port backend
